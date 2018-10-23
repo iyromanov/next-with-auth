71547c8836bb32530d39d52e0f2c1ecb15b706e2
Igor Romanov <ihar.ramanau@btsdigital.kz>
2018-10-23 18:01:42 +0600

    refactor: then => await

--- server/index.js
+++ server/index.js
@@ -1,52 +1,52 @@
-const express = require('express')
-const next = require('next')
+const express = require('express');
+const next = require('next');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
 
-app.prepare()
-  .then(() => {
-    const server = express()
-    server.use(bodyParser.urlencoded());
-    server.use(cookieParser());
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-    server.post('/auth', (req, res) => {
-      if (isUser(req.body)) {
-        res.setHeader('Set-Cookie', 'foo=bar; HttpOnly');
-      }
-      return app.render(req, res, '/user');
-    });
+async function start() {
+  await app.prepare();
 
-    server.get('/me', (req, res) => {
-      console.log('cookies /me:');
-      console.log(req.cookies);
-      if (isAuthorized(req.cookies)) {
-        res.setHeader('Content-Type', 'application/json');
-        return res.send(JSON.stringify({ name: 'Igor' }));
-      } else {
-        return res.status(401).send({});
-      }
-    });
-
-    server.get('*', (req, res) => {
-      return handle(req, res)
-    })
-
-    server.listen(port, (err) => {
-      if (err) throw err
-      console.log(`> Ready on http://localhost:${port}`)
-    })
+  const server = express();
+  server.use([bodyParser.urlencoded(), cookieParser()]);
+  
+  server.post('/auth', (req, res) => {
+    if (isUser(req.body)) {
+      res.cookie('foo', 'bar', { httpOnly: true });
+    }
+    return app.render(req, res, '/user');
   });
+  
+  server.get('/me', (req, res) => {
+    if (isAuthorized(req.cookies)) {
+      res.setHeader('Content-Type', 'application/json');
+      return res.send(JSON.stringify({ name: 'Igor' }));
+    } else {
+      return res.status(401).send({});
+    }
+  });
+  
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+  
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+}
 
+start();
 
-  function isUser({ login, password }) {
-    return login === password;
-  }
+function isUser({ login, password }) {
+  return login === password;
+}
 
-  function isAuthorized({ foo }) {
-    return foo === 'bar';
-  }
+function isAuthorized({ foo }) {
+  return foo === 'bar';
+}
